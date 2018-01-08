fda7c645d802510233ba67164da52e94d9372ba9
guohz <Leslie_GuoGuo@163.com>
2018-01-08 18:24:42 +0800

    test spring boot

--- test/src/main/java/com/cechr/test/TestApplication.java
+++ test/src/main/java/com/cechr/test/TestApplication.java
@@ -2,10 +2,19 @@ package com.cechr.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@Controller
+@RequestMapping("/test")
 public class TestApplication {
 
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "hello";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
