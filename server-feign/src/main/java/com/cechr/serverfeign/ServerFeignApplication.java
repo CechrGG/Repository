231419f8f64ff9296b802ecf70db24c8474e0ddd
guohz <Leslie_GuoGuo@163.com>
2018-01-16 15:13:28 +0800

    test spring cloud feign

---
+++ server-feign/src/main/java/com/cechr/serverfeign/ServerFeignApplication.java
@@ -0,0 +1,16 @@
+package com.cechr.serverfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class ServerFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerFeignApplication.class, args);
+	}
+}
---
+++ server-feign/src/main/java/com/cechr/serverfeign/ServiceHello.java
@@ -0,0 +1,12 @@
+package com.cechr.serverfeign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value="service-hello")
+public interface ServiceHello {
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	String sayHello(@RequestParam(value="name")String name);
+}
