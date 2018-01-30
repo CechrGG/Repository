9cbecdf97658053388377477fdbe47a15540bda8
guohz <Leslie_GuoGuo@163.com>
2018-01-30 18:10:42 +0800

    test spring boot with mybatis

---
+++ server-mybatis/src/main/java/com/cechr/servermabatis/ServerMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.cechr.servermabatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerMybatisApplication.class, args);
+	}
+}
