457e583e6260dc838d30e80e403970052f30cbb9
guohz <Leslie_GuoGuo@163.com>
2018-01-31 16:15:52 +0800

    test spring boot with mybatis

--- server-mybatis/src/main/java/com/cechr/servermybatis/DeviceController.java
+++ server-mybatis/src/main/java/com/cechr/servermybatis/DeviceController.java
@@ -1,5 +1,21 @@
 package com.cechr.servermybatis;
 
-public class DeviceController {
+import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeviceController {
+	
+	@Resource
+	private DeviceService devService;
+	
+	@RequestMapping("/getDevices")
+	public List<Device> getDevicesByName(@RequestParam(value = "name") String name) {
+		return devService.getDevicesByName(name);
+	}
 }
