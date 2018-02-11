f5775d222f157fab0c9e02e523115d7e8e68e29f
guohz <Leslie_GuoGuo@163.com>
2018-02-11 16:59:57 +0800

    fix a warning

--- server-kafka/src/main/java/com/cechr/serverkafka/ServerKafkaApplication.java
+++ server-kafka/src/main/java/com/cechr/serverkafka/ServerKafkaApplication.java
@@ -1,6 +1,5 @@
 package com.cechr.serverkafka;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
